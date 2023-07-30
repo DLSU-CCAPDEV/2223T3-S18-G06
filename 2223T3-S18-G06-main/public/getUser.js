94b6d798031290534407a08695e0659d37d897bb
robzjgman <106811460+robzjgman@users.noreply.github.com>
2023-07-30 23:31:46 +0800

    Add files via upload

---
+++ 2223T3-S18-G06-main/public/getUser.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('.post-username').click(function(event) {
+        event.preventDefault();
+
+        var currUser = $('#currentUser').text();
+        var username = $(this).data('username');
+
+        console.log('Current User here: ', currUser);
+        console.log('  : ', username);
+
+        $.get('/View_Profile/' + username, { currUser: currUser }, function(result) {
+            window.location.href = '/View_Profile/' + username + '?currUser=' + currUser;
+        });
+    });
+
+    $('.post-title').click(function(event) {
+        event.preventDefault();
+
+        var username = $(this).data('username');
+        var title = $(this).data('title');  
+        var currUser = $('#currentUser').text();
+
+        $.get('/View_Post/' + username + '/' + title, {currUser: currUser}, function(){
+            window.location.href = '/View_Post/' + username + '/' + title + '?currUser=' + currUser;
+        });
+    });
+});
